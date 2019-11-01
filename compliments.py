26ac21e44fcdbf54cd66bc87496801fce7960cca
Jeremy Schmidt <8888694+nocturnalBadger@users.noreply.github.com>
2019-11-01 18:40:43 -0400

    Remove fstring to support older python

--- compliments.py
+++ compliments.py
@@ -11,7 +11,7 @@ MM_HOME = os.environ.get("MAGICMIRROR_HOME")
 def write_compliments_file(count):
     rhymes = [rhyming.find_rhyme(3, "bar") for _ in range(count)]
 
-    messages = [f"♫ Break me off a piece of that {rhyme} ♫" for rhyme in rhymes]
+    messages = ["♫ Break me off a piece of that %s ♫" % rhyme for rhyme in rhymes]
     compliments_json = {"anytime": messages}
 
     file_path = os.path.join(MM_HOME, "modules", "default", "compliments", "compliments.json")
